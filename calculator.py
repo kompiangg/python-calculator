95bbc60b77a402d8f2ac66be24641f8210d682ac
Kompiang Gede Sukadharma <51501141+kompiangg@users.noreply.github.com>
2021-03-04 00:40:07 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,7 +3,8 @@ import re
 print("Welcome to the python calculator"
     "\nwhere you can type equation continously"
     "\nin python operator syntax and get the result\n"
-    "\nType quit or exit to close\n"
+    "\nType quit or exit to close"
+    "\nType reset to reset the result\n"
     "\nREMINDER : finish the equation before calculate")
 
 result = 0
@@ -19,6 +20,8 @@ while (True) :
     if (equation == 'quit') | (equation == 'exit'):
         print("Thank you for using this calculator, bruh")
         break
+    elif equation == 'reset':
+        result = 0
     else :
         equation = re.sub("[^0-9\+\*\/\-\().]", "", equation)
         result = str(eval(result + equation))
